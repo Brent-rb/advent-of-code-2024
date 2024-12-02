0fe01725c814c2c3e077ce95dc73219988dbe31a
Brent Berghmans <brent.berghmans@gmail.com>
2024-12-02 11:14:58 +0100

    feat: refactor day 1

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -1,40 +1,30 @@
 package be.brentberghmans.advent2024.days
 
+import be.brentberghmans.advent2024.extensions.toIntList
 import be.brentberghmans.advent2024.models.Day
-import be.brentberghmans.advent2024.utils.ListUtils
 import kotlin.math.abs
+import kotlin.math.cos
 
 class Day01(): Day {
     override fun solveA(data: List<String>): String {
-        val numberEntries = ListUtils.getVerticalLists(data, "   ") {
-            it.toInt()
-        }
-
-        val leftList = numberEntries.map { it[0] }.sorted()
-        val rightList = numberEntries.map { it[1] }.sorted()
-
-        val differences = leftList.mapIndexed { index, entry ->
-            abs(entry - rightList[index])
+        val rows = data.map { it.toIntList("   ") }
+        val column0 = rows.map { it[0] }.sorted()
+        val column1 = rows.map { it[1] }.sorted()
+        val differences = rows.indices.map {
+            abs(column0[it] - column1[it])
         }
 
         return differences.sum().toString()
     }
 
     override fun solveB(data: List<String>): String {
-        val numberEntries = ListUtils.getVerticalLists(data, "   ") {
-            it.toInt()
-        }
+        val rows = data.map { it.toIntList("   ") }
+        val column0 = rows.map { it[0] }
+        val column1 = rows.map { it[1] }
+        val occurrences = column1.groupingBy { it }.eachCount()
 
-        val leftList = numberEntries.map { it[0] }
-        val rightList = numberEntries.map { it[1] }
-
-        val rightListOccurrences = mutableMapOf<Int, Int>()
-        rightList.forEach {
-            rightListOccurrences[it] = (rightListOccurrences[it] ?: 0) + 1
-        }
-
-        val solution = leftList.sumOf {
-            it * (rightListOccurrences[it] ?: 0)
+        val solution = column0.sumOf {
+            it * (occurrences[it] ?: 0)
         }
 
         return solution.toString()
