1339f831e7aa5e96a16bf8f1ca6f022b328e3c4b
Brent Berghmans <brent.berghmans@gmail.com>
2024-12-03 21:15:03 +0100

    refactor: changed some to use firstOrNull

--- src/main/kotlin/extensions/ListExtensions.kt
+++ src/main/kotlin/extensions/ListExtensions.kt
@@ -1,5 +1,5 @@
 package be.brentberghmans.advent2024.extensions
 
 fun <T> List<T>.some(predicate: (T) -> Boolean): Boolean {
-    return find(predicate) != null
+    return firstOrNull(predicate) != null
 }
